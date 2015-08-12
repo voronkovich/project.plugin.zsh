8ab2df33cb0c6a8dd25f91075fe6c97900c50bba
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2015-08-12 19:09:22 +0300

    Fix autocompletion

--- project.plugin.zsh
+++ project.plugin.zsh
@@ -4,4 +4,8 @@ function p() {
     [[ -d $PROJECTS/$1 ]] && cd $PROJECTS/$1 || mkdir $PROJECTS/$1 && cd $PROJECTS/$1;
 }
 
-compdef '_files -W $PROJECTS' p
+function _p() {
+    _files -W $PROJECTS
+}
+
+compdef _p p
