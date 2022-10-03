4de344b060991466ec649125b1459ae6c8fe0fbb
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-10-03 22:08:18 +0300

    Refactor and improve CS

--- project.plugin.zsh
+++ project.plugin.zsh
@@ -1,33 +1,93 @@
-#!/usr/bin/env zsh
+PROJECTS="${PROJECTS:-$HOME/projects}"
+PROJECTS_TMP="${PROJECTS_TMP:-/tmp/${USER}_projects}"
+PROJECTS_RECIPES="${PROJECTS_RECIPES:-${PROJECTS}/.recipes}"
 
-PROJECTS="${PROJECTS:-$HOME/projects}";
-PROJECTS_TMP="${PROJECTS_TMP:-/tmp/${USER}_projects}";
-PROJECTS_RECIPES="${PROJECTS_RECIPES:-${PROJECTS}/.recipes}";
+hash -d p="${PROJECTS}"
+hash -d pt="${PROJECTS_TMP}"
 
-hash -d p="$PROJECTS";
-hash -d pt="$PROJECTS_TMP";
+alias p='project'
 
-function p() {
-    zparseopts -D {h,-help}=help {t,-temporary}=tmp {l,-list}=list {r,-recipe}:=recipe || return 1;
+project() {
+    local opt opt_help opt_tmp opt_list opt_recipe
 
-    for o in $@; do
-        if [[ ${o[1]} == '-' ]]; then
-            echo "Unknown option: $o." >&2;
-            return 1;
+    zparseopts -D \
+        {h,-help}=opt_help \
+        {t,-temporary}=opt_tmp \
+        {l,-list}=opt_list \
+        {r,-recipe}:=opt_recipe \
+        || return 1
+
+    for opt; do
+        if [[ "${opt[1]}" == '-' ]]; then
+            echo "Unknown option: ${opt}." >&2
+            return 1
         fi
-    done;
-    if [[ -n $recipe && ${recipe[2][1]} == '-' ]]; then
-        echo "Recipe option requires a value." >&2;
-        return 1;
+    done
+
+    if [[ -n "${opt_recipe}" && "${opt_recipe[2][1]}" == '-' ]]; then
+        echo "Recipe option requires a value." >&2
+        return 1
     fi
 
-    if [[ -n $help ]]; then
-        echo -en "
+    if [[ -n "${opt_help}" ]]; then
+        __project_help
+        return
+    fi
+
+    local projects_dir="${PROJECTS}"
+    if [[ -n "${opt_tmp}" ]]; then
+        projects_dir="${PROJECTS_TMP}"
+    fi
+
+    if [[ -n "${opt_list}" ]]; then
+        ls "${projects_dir}"
+        return
+    fi
+
+    local name="${1}"
+    local repo="${2}"
+    if [[ "${name}" =~ '^git@|https?://' ]]; then
+        repo="${name}"
+        name="${${repo##*/}%.git}"
+    fi
+
+    local recipe
+    if [[ -n "${opt_recipe}" ]]; then
+        recipe="${opt_recipe[2]}"
+
+        if [[ ! -n "${name}" ]]; then
+            echo "You can't use the recipes when the project name is not specified." >&2
+            return 1
+        fi
+
+        if [[ ! -x "${PROJECTS_RECIPES}/${recipe}" ]]; then
+            echo "Recipe \"${recipe}\" is not exist or not executable." >&2
+            return 1
+        fi
+    fi
+
+    if [[ ! -d "${projects_dir}/${name}" ]]; then
+        mkdir -p "${projects_dir}/${name}"
+    fi
+
+    cd "${projects_dir}/${name}"
+
+    if [[ -n "${repo}" ]]; then
+        git clone "${repo}" "${projects_dir}/${name}"
+    fi
+
+    if [[ -n "${recipe}" ]]; then
+        "${PROJECTS_RECIPES}/${recipe}"
+    fi
+}
+
+__project_help() {
+    echo -en "
 \e[32mProject manager\e[0m (\e[96m${PROJECTS}\e[0m)
 
 \e[33mUsage:\e[0m
 
-\e[32mp\e[0m [options] [project_name]
+    \e[32mp\e[0m [options] [project_name]
 
 \e[33mOptions:\e[0m
 
@@ -55,57 +115,11 @@ function p() {
     \e[37m# Lists all temporary projects\e[0m
     \e[32mp\e[0m -tl
 "
-        return 0;
-    fi
-
-    if [[ -n $tmp ]]; then
-        local dir="${PROJECTS_TMP}";
-    else
-        local dir="$PROJECTS";
-    fi
-
-    if [[ -n $list ]]; then
-        ls "$dir";
-        return 0;
-    fi
-
-    local name="$1";
-    if [[ "$name" =~ '^git@|https?://' ]]; then
-        local repo="$name";
-        local name="${${repo##*/}%.git}";
-    else
-        local repo="$2";
-    fi
-
-    if [[ -n "$recipe" ]]; then
-        recipe=$recipe[2];
-
-        if [[ ! -n "$name" ]]; then
-            echo "You can't use the recipes when the project name is not specified." >&2;
-            return 1;
-        fi
-
-        if [[ ! -x "$PROJECTS_RECIPES/$recipe" ]]; then
-            echo "Recipe \"$recipe\" is not exist or not executable." >&2;
-            return 1;
-        fi
-    fi
-
-    [[ ! -d "$dir/$name" ]] && mkdir -p "$dir/$name";
-    cd "$dir/$name";
-
-    [[ -n "$repo" ]] && git clone "$repo" "$dir/$name";
-
-    if [[ -n "$recipe" ]]; then
-        "$PROJECTS_RECIPES/$recipe";
-    fi
-
-    return 0;
 }
 
-function _p() {
-    local context state state_descr line ret=1;
-    typeset -A opt_args
+_project() {
+    local context state state_descr line ret=1
+    local -A opt_args
 
     _arguments -s -A '-h' -A '--help' \
         '(-h --help)'{-h,--help}'[Show help]' \
@@ -113,17 +127,17 @@ function _p() {
         '(-r --recipe)'{-r,--recipe}"[Use recipe (${PROJECTS_RECIPES})]:recipe:_files -W ${PROJECTS_RECIPES}" \
         '(-t --temporary)'{-t,--temporary}"[Create temporary project (${PROJECTS_TMP})]" \
         "::project name:->project" \
-    && ret=0;
+    && ret=0
 
-    if [[ "$state" == 'project' ]]; then
-        if [[ "$words" =~ '\s-\w*t' ]]; then
-            _files -W "${PROJECTS_TMP}";
+    if [[ "${state}" == 'project' ]]; then
+        if [[ "${words}" =~ '\s-\w*t' ]]; then
+            _files -W "${PROJECTS_TMP}"
         else
-            _files -W "${PROJECTS}";
+            _files -W "${PROJECTS}"
         fi
     fi
 
-    return ret;
+    return ret
 }
 
-compdef _p p;
+compdef _project project
